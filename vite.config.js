f363e6fb8bd5b6beb5e3b8a4137779197239547c
Nikita <nikita.derjugin@gmail.com>
2025-10-17 12:24:18 +0300

    gh-pages fix

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
     plugins: [react(), svgr()],
+    base: '/shiftWeb/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
